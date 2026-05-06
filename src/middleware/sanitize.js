dced2a2d4c2720ba1e6475dd4c892b45107fa4f8
carlaamendeez <carla.mendez.bonilla@gmail.com>
2026-05-06 16:21:34 +0200

    fix: middleware de sanitizacion

--- src/middleware/sanitize.js
+++ src/middleware/sanitize.js
@@ -1,3 +1,7 @@
 const mongoSanitize = require('express-mongo-sanitize')
 
-module.exports = mongoSanitize()
+module.exports = (req, res, next) => {
+    mongoSanitize.sanitize(req.body)
+    mongoSanitize.sanitize(req.params)
+    next()
+}
